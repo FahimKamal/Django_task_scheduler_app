53787261f5232347c3ea8569be389666c8393f21
FahimKamal <fahimkamal63@gmail.com>
2020-06-13 17:47:49 +0600

    3rd commit login user is now functional.

--- todowoo-project/todo/admin.py
+++ todowoo-project/todo/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import FamilyRole, Family, Person
 
 # Register your models here.
+class FamilyRoleInline(admin.TabularInline):
+    model = FamilyRole
+
+
+class FamilyInline(admin.TabularInline):
+    model = Family
+
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [FamilyRoleInline]
+
+
+admin.site.register(FamilyRole)
+admin.site.register(Family)
+admin.site.register(Person, PersonAdmin,)
--- todowoo-project/todo/models.py
+++ todowoo-project/todo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Family(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class FamilyRole(models.Model):
+    role = models.CharField(max_length=30)
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.role
+
+
+class Person(models.Model):
+    Name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    family = models.ForeignKey(FamilyRole, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return self.Name
+
