32dfcdc11d441655ef2366210ee2806b7a6b24bf
FahimKamal <fahimkamal63@gmail.com>
2020-06-13 12:37:07 +0600

    2nd commit Sign up for new user is added.

--- todowoo-project/todo/views.py
+++ todowoo-project/todo/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
 
 # Create your views here.
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        # Create a new User
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password2'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {
+                    'form': UserCreationForm(),
+                    'error': 'That username has already been taken. Please choose a new username.',
+                })
+        else:
+            # Tell the user the passwords didn't match
+            print('Password did not match')
+            return render(request, 'todo/signupuser.html', {
+                                                            'form': UserCreationForm(),
+                                                            'error': 'Password did not match',
+                                                            })
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
